7c8f0ebb5f1c691f07166625d7467efe4dfbece1
kosshylo <kostiantyn.shylo@gmail.com>
2025-03-24 16:14:55 +0000

    corrected misinterpreted mistake in task 1

--- lesson_03/homework_03.py
+++ lesson_03/homework_03.py
@@ -1,4 +1,5 @@
 #alice_in_wonderland: str = '"Would you tell me, please, which way I ought to go from here?"\n"That depends a good deal on where you want to get to," said the Cat.\n"I don\'t much care where ——" said Alice.\n"Then it doesn\'t matter which way you go," said the Cat.\n"—— so long as I get somewhere," Alice added as an explanation.\n"Oh, you\'re sure to do that," said the Cat, "if you only walk long enough."'
+
 # task 01 == Розділіть змінну alice_in_wonderland так, щоб вона займала декілька фізичних лінії
 alice_in_wonderland: str = (
     '"Would you tell me, please, which way I ought to go from here?"\n'
