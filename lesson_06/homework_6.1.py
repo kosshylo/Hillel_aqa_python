254be89f56a2b2bc927ee6ef87ff4e62d7580eea
kosshylo <kostiantyn.shylo@gmail.com>
2025-03-30 14:30:51 +0100

    # fixed according to comment 30.03.2025

--- lesson_06/homework_6.1.py
+++ lesson_06/homework_6.1.py
@@ -1,10 +1,13 @@
 '''Порахувати кількість унікальних символів в строці. Якщо їх більше 10 - вивести в консоль True,
 інакше - False. Строку отримати за допомогою функції input().'''
+from pickle import FALSE
+
 print("_" * 100)
 print("\n task_6.1:")
 
 entred_str: str = input("Please print your string: ")
 str_to_set = set(entred_str)
 unic_elements = len(str_to_set)
+answer: bool = unic_elements >10
 
-print(f'Кількість унікальних символів у введеному рядку: "{unic_elements}"' )
+print(f'"{answer}": Унікальних символів > 10')
