6206ae79794161c6bd02322aa0980875d271eb09
kosshylo <kostiantyn.shylo@gmail.com>
2025-05-08 22:21:09 +0100

    # initialization average_mark to 0 by defolt

--- lesson_14/homework_14/student.py
+++ lesson_14/homework_14/student.py
@@ -1,10 +1,16 @@
 class Student:
 
-    def __init__(self, name: str, surname: str, age: int, average_mark: int):
+    def __init__(self, name: str, surname: str, age: int):
         self.name = name
         self.surname = surname
         self.age = age
-        self.average_mark = average_mark
+        self.__settings_average_mark_to_0()
+
+    def return_average_mark(self):
+        return self.__average_mark
+
+    def __settings_average_mark_to_0(self):
+        self.__average_mark = 0
 
     def edit_average_mark(self, new_average_mark: int):
         '''
@@ -12,10 +18,10 @@ class Student:
         :param new_average_mark: new value
         :return: edited average mark
         '''
-        self.average_mark = new_average_mark
+        self.__average_mark = new_average_mark
 
     def show_info(self):
         print(f' Name:\t             {self.name}')
         print(f' Surname:\t         {self.surname}')
         print(f' Age:\t             {self.age}')
-        print(f' Average mark:\t     {self.average_mark}')
+        print(f' Average mark:\t     {self.__average_mark}')
--- lesson_14/homework_14/work_with_student.py
+++ lesson_14/homework_14/work_with_student.py
@@ -1,6 +1,8 @@
 from lesson_14.homework_14.student import Student
 
-kostia: Student = Student(name='Kostia', surname='Shylo', age=35, average_mark=95)
+kostia: Student = Student(name='Kostia', surname='Shylo', age=35)
+print(f'\nAfter changed avarage mark:')
+kostia.show_info()
 
 kostia.edit_average_mark(97)
 print(f'\nAfter changed avarage mark:')
